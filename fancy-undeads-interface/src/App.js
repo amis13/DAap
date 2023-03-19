a801696dc19678971db3580040fd2776c1836fee
Ismael <lindoespinoso@gmail.com>
2023-03-19 05:44:39 +0100

    v.0.0.1

--- fancy-undeads-interface/src/App.js
+++ fancy-undeads-interface/src/App.js
@@ -1,5 +1,5 @@
-import { Route, Routes } from “react-router-dom”;
-import Home from “./views/home”;
+import { Route, Routes } from "react-router-dom";
+import Home from "./views/home";
 
 function App() {
 return (
