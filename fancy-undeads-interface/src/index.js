ebcc8c019c1f6a2bd0c59d46a48049fc8126b7b8
Ismael <lindoespinoso@gmail.com>
2023-03-19 05:47:13 +0100

    v.0.0.1

--- fancy-undeads-interface/src/index.js
+++ fancy-undeads-interface/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
 import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
