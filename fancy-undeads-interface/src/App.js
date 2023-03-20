2a3a066875fc973dc6654647649cc52761f5269f
Ismael <lindoespinoso@gmail.com>
2023-03-20 01:01:24 +0100

    v.0.0.1

--- fancy-undeads-interface/src/App.js
+++ fancy-undeads-interface/src/App.js
@@ -1,13 +1,27 @@
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { Route } from "react-router-dom";
 import Home from "./views/home";
+import Web3 from "web3/dist/web3.min";
 
 function App() {
-return (
+  useEffect(() => {
+    if (window.ethereum) {
+      // window.ethereum
+      //   .request({
+      //     method: "eth_requestAccounts",
+      //   })
+      //   .then((accounts) => console.log(accounts));
 
-  <Routes>
-  <Route path="/" exact element={<Home/>} />
-  </Routes>
-);
+      const web3 = new Web3(window.ethereum);
+      web3.eth.requestAccounts().then(console.log);
+    }
+  }, []);
+
+  return (
+    <>
+      <Route path="/" exact component={Home} />
+    </>
+  );
 };
 
 export default App;
