4ac5d141d39a84a864717b3a3a931473e58794e7
Ismael <lindoespinoso@gmail.com>
2023-03-21 05:58:04 +0100

    v.0.0.2

--- platzi-punks-interface/src/App.js
+++ platzi-punks-interface/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./views/home";
 import Punks from "./views/punks";
+import Punk from "./views/punk";
 import MainLayout from "./layouts/main";
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Home/>} />
           <Route path="/punks" exact component={<Punks/>} />
-          <Route path="/punks/:tokenId" exact component={<Punks/>} />
+          <Route path="/punks/:tokenId" exact component={<Punk/>} />
         </Routes>
      </MainLayout>
   );
