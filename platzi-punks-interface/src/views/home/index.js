3d5fd3347dabc0b7261311e41eb257eb0e443238
Ismael <lindoespinoso@gmail.com>
2023-03-21 04:36:28 +0100

    v.0.0.2

--- platzi-punks-interface/src/views/home/index.js
+++ platzi-punks-interface/src/views/home/index.js
@@ -1,9 +1,31 @@
+import { useWeb3React } from "@web3-react/core";
+import { useCallback, useEffect, useState } from "react";
+import usePlatziPunks from "../../hooks/usePlatziPunks";
+
 const Home = () => {
-    return (
-      <>
-        <p>Hello world </p>
-      </>
-    );
-  };
-  
-  export default Home;
+  const { active } = useWeb3React();
+  const [maxSupply, setMaxSupply] = useState();
+
+  const platziPunks = usePlatziPunks();
+
+  const getMaxSupply = useCallback(async () => {
+    if (platziPunks) {
+      const result = await platziPunks.methods.maxSupply().call();
+      setMaxSupply(result);
+    }
+  }, [platziPunks]);
+
+  useEffect(() => {
+    getMaxSupply();
+  }, [getMaxSupply]);
+
+  if (!active) return "Conecta tu wallet";
+
+  return (
+    <>
+      <p>Max supply: {maxSupply}</p>
+    </>
+  );
+};
+
+export default Home;
