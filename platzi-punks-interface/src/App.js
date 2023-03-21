ca4224b1564ad34227db1f81b92da2671a7c22df
Ismael <lindoespinoso@gmail.com>
2023-03-21 05:42:07 +0100

    v.0.0.2

--- platzi-punks-interface/src/App.js
+++ platzi-punks-interface/src/App.js
@@ -9,7 +9,8 @@ function App() {
       <MainLayout>
         <Routes>
           <Route path="/" exact element={<Home/>} />
-          <Route path="/punks" exact component={Punks} />
+          <Route path="/punks" exact component={<Punks/>} />
+          <Route path="/punks/:tokenId" exact component={<Punks/>} />
         </Routes>
      </MainLayout>
   );
